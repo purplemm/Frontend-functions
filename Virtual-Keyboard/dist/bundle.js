b3081ebe7eac7a271e990fbf209bc62e600988a8
노보람 <boramm316@gmail.com>
2022-04-25 23:18:58 +0900

    Modify plugin settings, html, css, js

--- Virtual-Keyboard/dist/bundle.js
+++ Virtual-Keyboard/dist/bundle.js
@@ -1,2 +1,5 @@
-(()=>{"use strict";console.log(3)})();
+(() => {
+  "use strict";
+  console.log(3);
+})();
 //# sourceMappingURL=bundle.js.map
--- Virtual-Keyboard/webpack.config.js
+++ Virtual-Keyboard/webpack.config.js
@@ -5,43 +5,40 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    entry: "./src/js/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "./dist"),
-        clean: true
-    },
-    devtool: "source-map",
-    mode: "development",
-    devServer: {
-        host: "localhost",
-        port: 8080,
-        open: true,
-        watchFiles: "index.html"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Keyboard",
-            template: "./index.html",
-            inject: "body",
-            // favicon: "./favicon.ico"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-        })
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    open: true,
+    watchFiles: "index.html",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Virtual Keyboard",
+      template: "./index.html",
+      inject: "body",
+      // favicon: "./favicon.ico"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            }
-        ]
-    },
-    optimization: {
-        minimizer: [
-            new TerserkPlugin(),
-            new CssMinimizerPlugin()
-        ]
-    }
-}
+  },
+  optimization: {
+    minimizer: [new TerserkPlugin(), new CssMinimizerPlugin()],
+  },
+};
